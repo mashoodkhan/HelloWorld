76a8e55d06967e048c0722f483e26e348199fec8
mashoodkhan <70470042+mashoodkhan@users.noreply.github.com>
2024-01-31 14:54:10 +0000

    Add files via upload

---
+++ src/Test/HelloWorld.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class HelloWorld {
+	public static void helloWorld() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		helloWorld();
+	}
+}
